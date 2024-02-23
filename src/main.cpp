80f964aebbab92b32ea6ba4f997968d1548abce9
Daan Verbeek <daanverbeek13@gmail.com>
2024-02-23 09:59:46 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  Serial.println("Hello World");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // Serial.println("hoi ik ben daan en dit staat in de loop.");
+}
