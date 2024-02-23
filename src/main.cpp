03f2900108d2246b0c4694151c16738032e6ea98
Daan Verbeek <daanverbeek13@gmail.com>
2024-02-23 13:05:46 +0100

    added buttons

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 
-
 const int PIN_LED_RED = 4;    // The Number of the red LED pin.
 const int PIN_LED_GREEN = 5;  // The Number of the green LED pin.
 const int PIN_LED_BLUE = 6;   // The Number of the blue LED pin.
@@ -19,7 +18,10 @@ void setup() {
 
   pinMode(PIN_LED_YELLOW, OUTPUT);
   pinMode(PIN_LED_GREEN, OUTPUT);
-
+  pinMode(PIN_LED_RED, OUTPUT);
+  pinMode(PIN_LED_BLUE, OUTPUT);
+  pinMode(PIN_BUTTON_1, INPUT_PULLUP);
+  pinMode(PIN_BUTTON_2, INPUT_PULLUP);
 
 }
 unsigned long myTime;
@@ -35,16 +37,31 @@ void toggleLED(int LED) {
   digitalWrite(LED, HIGH);
 }
 
+void buttonHandler(int BUTTON_PIN, int LED_PIN) {
+  const int buttonState = digitalRead(BUTTON_PIN);
+
+  if (buttonState == LOW) {
+    digitalWrite(LED_PIN, HIGH);
+  }  else {
+    digitalWrite(LED_PIN, LOW);
+  }
+}
+
 void loop() {
-  delay(1000);
-  toggleLED(PIN_LED_YELLOW);
-  delay(1000);
-  toggleLED(PIN_LED_YELLOW);
-  delay(1000);
-  toggleLED(PIN_LED_GREEN);
-  delay(1000);
-  toggleLED(PIN_LED_GREEN);
-  // Serial.print("Time: ");
+  // delay(1000);
+  // toggleLED(PIN_LED_YELLOW);
+  // delay(1000);
+  // toggleLED(PIN_LED_YELLOW);
+  // delay(1000);
+  // toggleLED(PIN_LED_GREEN);
+  // delay(1000);
+  // toggleLED(PIN_LED_GREEN);
+
+  buttonHandler(PIN_BUTTON_1, PIN_LED_BLUE);
+  buttonHandler(PIN_BUTTON_2, PIN_LED_RED);
+  
+}
+
+ // Serial.print("Time: ");
   // myTime = millis();
   // Serial.println(myTime); // prints time since program started
-}
